de99db662255cc5a47642980840276aa69e48cdd
Malgier <stevo730@hotmail.co.uk>
2017-11-10 15:47:27 +0000

    Add files via upload

---
+++ FrontEndApp/FrontEndApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FrontEndApp.Models;
+using System.Net.Http;
+
+namespace FrontEndApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HttpClient client = new HttpClient();
+
+        public IActionResult Index()
+        {
+            client.BaseAddress = new Uri("http://localhost:54330");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+            HttpResponseMessage response = client.GetAsync("/Products/Index").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                ProductVM vm = new ProductVM();
+                vm.ProductsPartialView = response.Content.ReadAsStringAsync().Result;
+                return View(vm);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FrontEndApp/FrontEndApp/Models/ProductVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEndApp.Models
+{
+    public class ProductVM
+    {
+        public string ProductsPartialView { get; set; }
+
+        public ProductVM()
+        {
+
+        }
+    }
+}
